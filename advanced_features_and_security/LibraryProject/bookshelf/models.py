477226d2a31faa8564de6bfcc87cb6786792d568
Repher Akello <akellorepher@gmail.com>
2024-11-27 15:26:35 +0300

    adding the books changes on models file

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    pages = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
 class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
