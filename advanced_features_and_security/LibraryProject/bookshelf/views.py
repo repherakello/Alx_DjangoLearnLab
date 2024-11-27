defc5788c4bfe4680a10ed407cafb88306e768e7
Repher Akello <akellorepher@gmail.com>
2024-11-27 16:36:06 +0300

    adding the exampleform

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -11,6 +11,19 @@ def search_books(request):
     # Secure way to filter books
     books = Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query))
     return render(request, 'bookshelf/book_list.html', {'books': books})
+from django.shortcuts import render, redirect
+from .forms import BookForm
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+
+    return render(request, 'bookshelf/add_book.html', {'form': form})
 
 
 
