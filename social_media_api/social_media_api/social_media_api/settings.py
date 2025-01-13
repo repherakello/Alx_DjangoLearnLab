b9ba61cf97c484db8d0b1452a9f1e7f960aac85f
Repher Akello <akellorepher@gmail.com>
2025-01-13 22:54:58 +0300

    adding the socila_media_api

--- social_media_api/social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/social_media_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
